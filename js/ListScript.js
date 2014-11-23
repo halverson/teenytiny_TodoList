08cd62e62d912e4e5ce9e7b1a70b67329bb26179
Brent Nygaard <nygaardmeister@gmail.com>
2014-11-23 22:13:39 +0200

    Add jquery and style more.

---
+++ js/ListScript.js
@@ -0,0 +1,12 @@
+var addItem = function() {
+  $('.mainList').append('<li class="listItem"><input class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="..."></li>');
+};
+
+var remItem = function() {
+  var isChecked = $("input[type='checkbox']:checked");
+  isChecked.parent().remove();
+};
+
+$('.addItem').click(addItem);
+
+$('.updateList').click(remItem);

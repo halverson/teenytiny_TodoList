39e12d1699e30fa1f11810ff33335ce4afa94196
Brent Nygaard <nygaardmeister@gmail.com>
2014-11-25 16:14:34 +0200

    Redesign layout and restyle.

--- js/ListScript.js
+++ js/ListScript.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
 
     var addItem = function() {
-      $('.mainList').append('<li class="listItem"><input class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="..."></li>');
-    };
-
-    var remItem = function() {
-      var isChecked = $("input[type='checkbox']:checked");
-      isChecked.parent().remove();
+      $('.mainList').append('<li class="listItem"><input id="check" class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="..."></li>');
     };
 
     $('.addItem').click(addItem);
-
-    $('.updateList').click(remItem);
-    
 });

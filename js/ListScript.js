50b082738f7c8fbf790ef1cb2ac927b58e90ccee
Brent Nygaard <nygaardmeister@gmail.com>
2014-11-26 09:20:33 +0200

    Identify problem with check selector in js.

--- js/ListScript.js
+++ js/ListScript.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
     var addItem = function() {
       $('.mainList').append('<li class="listItem"><input id="check" class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="..."></li>');
     };
-
+    
+    /*var donE = function() {
+        var checkED = $("input[type='checkbox']");
+        if (checkED.is(':checked')) {
+            checkED.parent().remove();
+        }
+    };*/
+    
+    var Massage = function() {
+        if ($(this).is(':checked')) {
+            alert("Checked!");
+        } else {
+            alert("Unchecked!");
+        }
+    };
+    
+    $('.mainList').on('click', '#check', Massage);
     $('.addItem').click(addItem);
 });

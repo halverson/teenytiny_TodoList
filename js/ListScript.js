07ee9f58931cdfbc7ba2f44e07ddf7fcc6bdc732
Brent Nygaard <nygaardmeister@gmail.com>
2014-11-26 11:55:52 +0200

    Make list items fade out on check.

--- js/ListScript.js
+++ js/ListScript.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
 
     var addItem = function() {
-      $('.mainList').append('<li class="listItem"><input id="check" class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="..."></li>');
+      $('.mainList').append('<li class="listItem"><input id="check" class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="Add Item..."></li>');
     };
     
     var itemDone = function() {
-        if ($(this).is(':checked')) {
-            alert("Checked!");
-        } else {
-            alert("Unchecked!");
-        }
+        var $this = $(this);
+        $this.parent().fadeOut(600, function() {
+            $this.parent().remove();
+        });
     };
     
     $('.mainList').on('click', '#check', itemDone);

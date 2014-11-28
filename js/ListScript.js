ded19a73ca3e5a85b7a6a81a9a2173895bdb70e6
Brent Nygaard <nygaardmeister@gmail.com>
2014-11-28 22:01:35 +0200

    Huge Changes
    Focusing on last text input now automatically adds another list item to
    the end of the list.
    
    Checkboxes are properly styled without too much hacking.
    
    Checking items moves them to a "done" list where the text is styled with
    a strikethrough. Items in the "done" list can be unchecked and moved
    back to the main list.

--- js/ListScript.js
+++ js/ListScript.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
 
     var addItem = function() {
-      $('.mainList').append('<li class="listItem"><div class="checkbox"><input id="check" class="checkBox" type="checkbox"><label for="check"></label></div><input class="textInp" type="text" placeholder="Add Item..."></li>');
+      $('.mainList').append('<li class="listItem"><input class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="Add Item..."><p class="remItem myBtn">x</p></li>');
+    };
+    
+    var delItem = function() {
+      $(this).parent().remove();  
     };
     
     var itemDone = function() {
-        var $this = $(this);
-        $this.parent().fadeOut(600, function() {
-            $this.parent().remove();
-        });
+        var itemDone = $(this).parent();
+        $('.doneList').append(itemDone);
     };
     
-    $('.mainList').on('click', '#check', itemDone);
-    $('.addItem').click(addItem);
+    var unDone = function() {
+        var notDone = $(this).parent();
+        $('.mainList').append(notDone);
+    };
+    
+    $('.mainList').on('click', '.checkBox', itemDone);
+    $('.doneList').on('click', '.checkBox', unDone);
+    $('.mainList').on('focus', 'li:last-child', addItem);
+    $('.mainList, .doneList').on('click', '.remItem', delItem);
 });

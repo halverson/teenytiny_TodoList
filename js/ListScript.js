bb8401cb7cf3c602a5d1391523ebd1a18bf555a8
Brent Nygaard <nygaardmeister@gmail.com>
2014-12-01 09:35:13 +0200

    Style changes and better deletion.

--- js/ListScript.js
+++ js/ListScript.js
@@ -3,21 +3,18 @@ $(document).ready(function() {
     var addItem = function() {
         var newItem = $('<li class="listItemA"><input class="checkBox" type="checkbox"><input class="textInp" type="text" placeholder="Add Item..."><p class="remItem myBtn">-</p></li>');
         $('.mainList').append(newItem);
-        newItem.fadeIn(500);
+        newItem.fadeIn(300);
     };
     
     var delItem = function() {
         var gonner = $(this).parent(),
             goneInp = gonner.find(':text').val();
         
-        if (gonner.is(':only-child')) {
-            gonner.fadeOut(100);
-            gonner.fadeIn(100);
-        } else if (goneInp === "") {
-            gonner.fadeOut(100);
-            gonner.fadeIn(100);
+        if (gonner.is(':last-child')) {
+            gonner.fadeOut(50);
+            gonner.fadeIn(50);
         } else {
-            gonner.fadeOut(500, function() {
+            gonner.fadeOut(300, function() {
                 gonner.remove();
             });
         }
@@ -27,12 +24,13 @@ $(document).ready(function() {
         var itemDone = $(this).parent(),
             doneInp = itemDone.find(':text').val();
         if (doneInp === "") {
-            itemDone.fadeOut(100);
-            itemDone.fadeIn(100);
+            itemDone.fadeOut(50);
+            itemDone.fadeIn(50);
+            $(this).attr("checked", false);
         } else {
-            itemDone.fadeOut(500, function() {
+            itemDone.fadeOut(300, function() {
                 $('.doneList').append(itemDone);
-                itemDone.fadeIn(500);
+                itemDone.fadeIn(300);
             });
         }
     };
@@ -40,9 +38,9 @@ $(document).ready(function() {
     var unDone = function() {
         var notDone = $(this).parent(),
             lastItem = $('.mainList li').last();
-        notDone.fadeOut(500, function() {
+        notDone.fadeOut(300, function() {
             lastItem.before(notDone);
-            notDone.fadeIn(500);
+            notDone.fadeIn(300);
         })
     };
     
